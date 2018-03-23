498933a001216b956020c19a4d5f3d74b53cda10
spookybird <wpoqwpoqw@gmail.com>
2018-03-23 13:02:37 +0900

    content

--- js/content.js
+++ js/content.js
@@ -32,11 +32,33 @@ funcs.loginForm = (form) => {
 }
 
 funcs.mainContent = (view, menu) => {
+  // 現在日付
+  const n = new Date();
+  const now = {
+    year: n.getFullYear(),
+    month: n.getMonth() + 1,
+    day: n.getDate(),
+  };
+
   view.frameElement.addEventListener('load', (e) => {
     const doc = e.target.contentWindow.document;
     const title = doc.title;
     if (title.startsWith('勤務表')) {
       // 勤務表ページ
+
+      // 表示している勤務表の年月
+      const d = doc.forms['dateForm'].date.value;
+      const viewYear = d.split('/')[0] - 0;
+      const viewMonth = d.split('/')[1] - 0;
+      let viewState;
+      if (now.year > viewYear || (now.year === viewYear && now.month > viewMonth)) {
+        viewState = 'PAST';
+      } else if (now.year === viewYear && now.month === viewMonth) {
+        viewState = 'CURRENT';
+      } else {
+        viewState = 'FUTURE';
+      }
+
       let table;
       for (let child of doc.body.childNodes) {
         if (child.textContent === '週／月切替') {
@@ -44,7 +66,75 @@ funcs.mainContent = (view, menu) => {
           break;
         }
       }
+      table.classList.add('alpha-attendance-table');
 
+      const rowDefinitions = [
+        {
+          header: '形態（予定）',
+          name: 'pattern',
+          innerTag: (row) => {
+            const tag = doc.createElement('select');
+            tag.classList.add('alpha-attendance-select-pattern');
+            if (isReadonly(viewState, row, now)) {
+              tag.setAttribute('readonly', 'readonly');
+            }
+            return tag;
+          },
+        },
+        {
+          header: '開始（予定）',
+          name: 'start',
+          innerTag: (row) => {
+            const tag = doc.createElement('input');
+            tag.classList.add('alpha-attendance-input-time');
+            if (isReadonly(viewState, row, now)) {
+              tag.setAttribute('readonly', 'readonly');
+            }
+            return tag;
+          },
+        },
+        {
+          header: '終了（予定）',
+          name: 'end',
+          innerTag: (row) => {
+            const tag = doc.createElement('input');
+            tag.classList.add('alpha-attendance-input-time');
+            if (isReadonly(viewState, row, now)) {
+              tag.setAttribute('readonly', 'readonly');
+            }
+            return tag;
+          },
+        },
+        {
+          header: '非請求時間（予定）',
+          name: 'unclaimed',
+          innerTag: (row) => {
+            const tag = doc.createElement('input');
+            tag.classList.add('alpha-attendance-input-time');
+            if (isReadonly(viewState, row, now)) {
+              tag.setAttribute('readonly', 'readonly');
+            }
+            return tag;
+          },
+        },
+      ];
+
+      const rows = Array.apply(null, table.querySelectorAll('tbody>tr'));
+      const headElement = rows.shift();
+      for (let definition of rowDefinitions) {
+        const th = doc.createElement('th');
+        th.textContent = definition.header;
+        headElement.appendChild(th);
+      }
+
+      for (let row of rows) {
+        for (let definition of rowDefinitions) {
+          const td = doc.createElement('td');
+          td.setAttribute('name', definition.name);
+          td.appendChild(definition.innerTag(row));
+          row.appendChild(td);
+        }
+      }
     }
   });
 }
@@ -57,4 +147,16 @@ if (form) {
   funcs.loginForm(form);
 } else if (mainFrame && menuFrame) {
   funcs.mainContent(mainFrame, menuFrame);
+}
+
+const isReadonly = (viewState, row, now) => {
+  if (viewState === 'FUTURE') return false;
+  if (viewState === 'PAST') return true;
+  if (viewState === 'CURRENT') {
+    const dateTag = row.querySelector('td:first-child > a');
+    const date = dateTag.textContent.match(/^\d+/) - 0;
+    if (now.day > date) return true;
+  }
+
+  return false;
 }
